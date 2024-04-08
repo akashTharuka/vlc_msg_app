8103a47692977b063eba4a45d8038f5bf5b56598
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-04-08 16:05:06 +0530

    db helper

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 import 'dart:developer';
-import 'dart:io' as io;
+// import 'dart:io' as io;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:rolodex/models/base_model.dart';
+// import 'package:vlc_msg_app/models/contact.dart';
+// import 'package:vlc_msg_app/models/msg.dart';
+// import 'package:vlc_msg_app/models/user.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
+
   static final DatabaseHelper _instance = DatabaseHelper.internal();
 
   factory DatabaseHelper() => _instance;
@@ -24,18 +26,37 @@ class DatabaseHelper {
   Future<Database> init() async {
 
     try {
-      io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
-      String path = join(documentsDirectory.path, "main.db");
-      return openDatabase(path, version: 1, onCreate: onCreate);
+
+      // io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+      // String path = join(documentsDirectory.path, "main.db");
+      String documentsDirectory = await getDatabasesPath();
+      String path = join(documentsDirectory, "main.db");
+      log(path);
+      return openDatabase(
+        path, 
+        version: 1, 
+        onCreate: onCreate
+      );
     } 
 		on MissingPlatformDirectoryException catch (e) {
+
 			log('Error: $e');
 			return Future.error(e);
     }
   }
 
-  void onCreate(Database db, int version) async =>
-      await db.execute('CREATE TABLE contacts (id INTEGER PRIMARY KEY NOT NULL, firstName STRING, lastName STRING, phone STRING, email STRING)');
+  void onCreate(Database db, int version) async {
+
+    await db.execute(
+      'CREATE TABLE user (name STRING NOT NULL, privateKey STRING NOT NULL, publicKey STRING NOT NULL, mobileUnlock BOOLEAN DEFAULT FALSE); CREATE TABLE contact (id STRING PRIMARY KEY NOT NULL, name STRING NOT NULL, publicKey STRING NOT NULL); CREATE TABLE messages (id STRING PRIMARY KEY, timestamp TIMESTAMP NOT NULL, text TEXT NOT NULL);'
+    );
+    // await db.execute(
+    //   'CREATE TABLE contact (id STRING PRIMARY KEY NOT NULL, name STRING NOT NULL, publicKey STRING NOT NULL)'
+    // );
+    // await db.execute(
+    //   'CREATE TABLE messages (id STRING PRIMARY KEY, timestamp TIMESTAMP NOT NULL, text TEXT NOT NULL)'
+    // );
+  }
 
   // Future<List<Map<String, dynamic>>> query(String table) async => (await db).query(table);
 
