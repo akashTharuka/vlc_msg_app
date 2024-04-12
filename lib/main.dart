bd4ff08c10d5c94047e9d431337fb5c501092805
akashtharuka <akash_tharuka@yahoo.com>
2024-04-12 21:06:49 +0530

    added a material app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:vlc_msg_app/pages/splash_screen.dart';
 
 void main() {
@@ -13,7 +14,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
+      title: 'LuminaLINQ',
+      theme: ThemeData(
+        useMaterial3: true,
+
+        // defeault brightness and colors
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple.shade50,
+          brightness: Brightness.dark,
+          background: const Color(0xffeef1f5),
+        ),
+
+        // default text theme
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w800,
+            color: Colors.black,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          displaySmall: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.black26,
+          ),
+        ),
+      ),
       home: SplashScreen(),
     );
   }
