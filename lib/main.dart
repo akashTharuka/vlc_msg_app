9ad6762a560b4ae1e957f771dcd1f86e5b399885
akashtharuka <akash_tharuka@yahoo.com>
2024-04-14 01:37:26 +0530

    custom textTheme and colorScheme added

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,69 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // custom text theme
+    final textTheme = ThemeData.dark().textTheme.copyWith(
+        titleLarge: const TextStyle(
+          fontSize: 57,
+          fontWeight: FontWeight.w400,
+          color: Color(0xff121212),
+        ),
+        titleMedium: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          color: Color(0xff121212),
+        ),
+        titleSmall: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: Color(0xff121212),
+        ),
+        bodyLarge: const TextStyle(
+          fontSize: 20,
+          color: Color(0xff121212),
+        ),
+        bodyMedium: const TextStyle(
+          fontSize: 16,
+          color: Color(0xff121212),
+        ),
+        bodySmall: const TextStyle(
+          fontSize: 14,
+          color: Color(0xff121212),
+        ),
+        labelLarge: const TextStyle(
+          fontSize: 20,
+          color: Color(0xff121212),
+        ),
+        labelMedium: const TextStyle(
+          fontSize: 16,
+          color: Color(0xff121212),
+        ),
+        labelSmall: const TextStyle(
+          fontSize: 14,
+          color: Color(0xff121212),
+        ));
+
+    const colorScheme = ColorScheme.dark(
+      primary: Color(0xff121212),
+      secondary: Color(0xff2f3135),
+      background: Color(0xffbcbcbc),
+      surface: Color(0xff4a545c),
+      error: Color(0xFFC7161F),
+      onPrimary: Color(0xff657b81),
+      onSecondary: Color(0xff83a4a4),
+      onSurface: Color(0xffa9cec2),
+      onError: Color(0xff1D1617),
+      brightness: Brightness.dark,
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'LuminaLINQ',
       theme: ThemeData(
         useMaterial3: true,
-
-        // defeault brightness and colors
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple.shade50,
-          brightness: Brightness.dark,
-          background: const Color(0xffeef1f5),
-        ),
-
-        // default text theme
         fontFamily: 'Poppins',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w800,
-            color: Colors.black,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            color: Colors.black,
-          ),
-          displaySmall: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-            color: Colors.black26,
-          ),
-        ),
-
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor:
-              Colors.black, // Change this color to your desired cursor color
-        ),
+        textTheme: textTheme,
+        colorScheme: colorScheme,
       ),
       home: const SplashScreen(),
     );
